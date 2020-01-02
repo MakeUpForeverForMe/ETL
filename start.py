dc09b55b146c511457c9a69827d5492b8db13950
魏喜明 <664651151@qq.com>
2020-01-02 15:20:01 +0800

    2020-01-02 主类获取数据库数据

--- start.py
+++ start.py
@@ -1,6 +1,24 @@
 from lib.MySQLConnect import MySQLConnect
+from json import loads
+from datetime import datetime
 
+print()
 with MySQLConnect() as con:
   con.execute('select * from ETL_STRUCT')
-  data = con.fetchall()
-  print(data)
+  datas = con.fetchall()
+  for data in datas:
+    print('alias\t', data['alias'])
+    print('imex\t', data['imex'])
+    print('type\t', data['type'])
+    print('hosts\t', data['hosts'])
+    print('user\t', data['user'])
+    print('passwd\t', data['passwd'])
+    print('fromdb\t', data['fromdb'])
+    print('aimsdb\t', data['aimsdb'])
+    print('table\t', data['table'])
+    print('stime\t', datetime.timetuple(data['stime']))
+    print('ltime\t', data['ltime'])
+    parti = None
+    if data['parti'] is not None:
+      parti = loads(data['parti'])
+    print('parti\t', parti)
