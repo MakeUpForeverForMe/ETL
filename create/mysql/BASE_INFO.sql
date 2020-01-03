775b9c17e285eedebec1db708e6eb06e893a7891
魏喜明 <664651151@qq.com>
2020-01-03 10:47:09 +0800

    2020-01-03 添加了用途类型，为了是基础表能够使用更多用途

--- create/mysql/BASE_INFO.sql
+++ create/mysql/BASE_INFO.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS `BASE_INFO` (
   `id`          bigint(20)    NOT NULL  AUTO_INCREMENT                  COMMENT '主键',
   `ctime`       datetime      NOT NULL  DEFAULT CURRENT_TIMESTAMP()     COMMENT '执行开始时间',
   `utime`       datetime      NOT NULL  DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP COMMENT '上次执行时间',
+  `id_type`     int(10)       NOT NULL  DEFAULT 0                       COMMENT '用途类型',
   `alias`       varchar(64)   DEFAULT NULL                              COMMENT '类型别名',
   `type`        varchar(64)   DEFAULT NULL                              COMMENT '数据来源类型',
   `hosts`       varchar(64)   DEFAULT NULL                              COMMENT '数据来源机器',
