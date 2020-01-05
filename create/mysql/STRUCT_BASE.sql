1eb26ccd704894dc5ea6d7bf8e4e0928b2e0eba8
魏喜明 <664651151@qq.com>
2020-01-05 13:58:35 +0800

    2020-01-05 表的id_type的默认值和注释修改后未保存，重新提交

--- create/mysql/STRUCT_BASE.sql
+++ create/mysql/STRUCT_BASE.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS `STRUCT_BASE`;
 
 CREATE TABLE IF NOT EXISTS `STRUCT_BASE` (
-  `id`          bigint(20)    NOT NULL  AUTO_INCREMENT                  COMMENT '主键',
+  `id`          BIGINT(20)    NOT NULL  AUTO_INCREMENT                  COMMENT '主键',
   `ctime`       datetime      NOT NULL  DEFAULT CURRENT_TIMESTAMP()     COMMENT '执行开始时间',
   `utime`       datetime      NOT NULL  DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP COMMENT '上次执行时间',
-  `id_type`     int(10)       NOT NULL  DEFAULT 0                       COMMENT '用途类型',
-  `alias`       varchar(64)   DEFAULT NULL                              COMMENT '类型别名',
-  `type`        varchar(64)   DEFAULT NULL                              COMMENT '数据来源类型',
-  `hosts`       varchar(64)   DEFAULT NULL                              COMMENT '数据来源机器',
-  `user`        varchar(64)   DEFAULT NULL                              COMMENT '数据来源用户',
-  `passwd`      varchar(64)   DEFAULT NULL                              COMMENT '数据来源密码',
-  `base_path`   varchar(256)  DEFAULT NULL                              COMMENT '数据来源根路径或库',
+  `id_type`     INT (10)      NOT NULL  DEFAULT '-1'                    COMMENT '用途类型：0、etl流程，1、etl系统',
+  `alias`       VARCHAR(64)   DEFAULT NULL                              COMMENT '类型别名',
+  `type`        VARCHAR(64)   DEFAULT NULL                              COMMENT '数据来源类型',
+  `hosts`       VARCHAR(64)   DEFAULT NULL                              COMMENT '数据来源机器',
+  `user`        VARCHAR(64)   DEFAULT NULL                              COMMENT '数据来源用户',
+  `passwd`      VARCHAR(64)   DEFAULT NULL                              COMMENT '数据来源密码',
+  `base_path`   VARCHAR(256)  DEFAULT NULL                              COMMENT '数据来源根路径或库',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT '基础信息表';
