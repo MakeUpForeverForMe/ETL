29ca78ec7c90a7df6ee1408d64b2e36ee54858d6
魏喜明 <664651151@qq.com>
2020-01-05 21:44:32 +0800

    2020-01-05 结构表增加了是否等待依赖表的任务完成在继续自己的任务

--- create/mysql/STRUCT_ETL.sql
+++ create/mysql/STRUCT_ETL.sql
@@ -1,18 +1,19 @@
 DROP TABLE IF EXISTS `STRUCT_ETL`;
 
 CREATE TABLE IF NOT EXISTS `STRUCT_ETL` (
-  `id`      BIGINT(20)    NOT NULL  AUTO_INCREMENT                  COMMENT '主键',
-  `ctime`   datetime      NOT NULL  DEFAULT CURRENT_TIMESTAMP()     COMMENT '执行开始时间',
-  `utime`   datetime      NOT NULL  DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP COMMENT '上次执行时间',
-  `alias`   VARCHAR(64)   DEFAULT NULL                              COMMENT '任务别名',
-  `rely`    VARCHAR(64)   DEFAULT NULL                              COMMENT '依赖任务',
-  `imex`    VARCHAR(64)   DEFAULT NULL                              COMMENT '执行阶段类型',
-  `base_id` BIGINT(20)    NOT NULL  DEFAULT 0                       COMMENT '基础信息表主键',
-  `from`    VARCHAR(256)  DEFAULT NULL                              COMMENT '数据来源表或文件路径',
-  `aims`    VARCHAR(64)   DEFAULT NULL                              COMMENT '数据目标路径或库',
-  `table`   VARCHAR(64)   DEFAULT NULL                              COMMENT '数据目标表名',
-  `stime`   datetime      NOT NULL  DEFAULT '2019-01-01 00:00:00'   COMMENT '执行开始时间',
-  `ltime`   datetime      NOT NULL  DEFAULT '2019-01-01 00:00:00'   COMMENT '上次执行时间',
-  `parti`   VARCHAR(256)  DEFAULT NULL                              COMMENT '分区字段',
+  `id`          BIGINT(20)    NOT NULL  AUTO_INCREMENT                  COMMENT '主键',
+  `ctime`       datetime      NOT NULL  DEFAULT CURRENT_TIMESTAMP()     COMMENT '执行开始时间',
+  `utime`       datetime      NOT NULL  DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP COMMENT '上次执行时间',
+  `alias`       VARCHAR(64)   DEFAULT NULL                              COMMENT '任务别名',
+  `rely`        VARCHAR(64)   DEFAULT NULL                              COMMENT '依赖任务',
+  `imex`        VARCHAR(64)   DEFAULT NULL                              COMMENT '执行阶段类型',
+  `base_id`     BIGINT(20)    NOT NULL  DEFAULT 0                       COMMENT '基础信息表主键',
+  `wait_rely`   INT(10)       NOT NULL  DEFAULT -1                      COMMENT '是否等待依赖任务完成',
+  `from`        VARCHAR(256)  DEFAULT NULL                              COMMENT '数据来源表或文件路径',
+  `aims`        VARCHAR(64)   DEFAULT NULL                              COMMENT '数据目标路径或库',
+  `table`       VARCHAR(64)   DEFAULT NULL                              COMMENT '数据目标表名',
+  `stime`       datetime      NOT NULL  DEFAULT '2019-01-01 00:00:00'   COMMENT '执行开始时间',
+  `ltime`       datetime      NOT NULL  DEFAULT '2019-01-01 00:00:00'   COMMENT '上次执行时间',
+  `parti`       VARCHAR(256)  DEFAULT NULL                              COMMENT '分区字段',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT 'ETL结构表';
