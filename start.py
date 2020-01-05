ac7442afa590aadaec57793b9b0cb326b4aae7f9
魏喜明 <664651151@qq.com>
2020-01-05 21:45:03 +0800

    2020-01-05 测试执行做了适当的修改

--- start.py
+++ start.py
@@ -3,24 +3,28 @@ from json import loads
 from datetime import datetime
 
 with MySQLConnect(host='192.168.18.100', passwd='000000') as con:
+  con.execute('select * from STRUCT_BASE')
+  datas = con.fetchall()
+  print(datas)
   con.execute('select * from STRUCT_ETL')
   datas = con.fetchall()
-  for data in datas:
-    print('alias\t', data['alias'])
-    print('imex\t', data['imex'])
-    print('type\t', data['type'])
-    print('hosts\t', data['hosts'])
-    print('user\t', data['user'])
-    print('passwd\t', data['passwd'])
-    print('fromdb\t', data['fromdb'])
-    print('aimsdb\t', data['aimsdb'])
-    print('table\t', data['table'])
-    print('stime\t', datetime.timetuple(data['stime']))
-    print('ltime\t', data['ltime'])
-    year_month = None
-    day_of_month = None
-    if data['parti'] is not None:
-      year_month = loads(data['parti'])['year_month']
-      day_of_month = loads(data['parti'])['day_of_month']
-    print('year_month\t', year_month)
-    print('day_of_month\t', day_of_month)
+  print(datas)
+  # for data in datas:
+  #   print('alias\t', data['alias'])
+  #   print('imex\t', data['imex'])
+  #   print('type\t', data['type'])
+  #   print('hosts\t', data['hosts'])
+  #   print('user\t', data['user'])
+  #   print('passwd\t', data['passwd'])
+  #   print('fromdb\t', data['fromdb'])
+  #   print('aimsdb\t', data['aimsdb'])
+  #   print('table\t', data['table'])
+  #   print('stime\t', datetime.timetuple(data['stime']))
+  #   print('ltime\t', data['ltime'])
+  #   year_month = None
+  #   day_of_month = None
+  #   if data['parti'] is not None:
+  #     year_month = loads(data['parti'])['year_month']
+  #     day_of_month = loads(data['parti'])['day_of_month']
+  #   print('year_month\t', year_month)
+  #   print('day_of_month\t', day_of_month)
