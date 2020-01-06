92aa0edb591a971ba857807f7844312ec60a7f78
魏喜明 <664651151@qq.com>
2020-01-07 00:59:22 +0800

    2020-01-07 增加了关闭连接的方法

--- src/main/scala/com/etl/utils/JDBCUtils.scala
+++ src/main/scala/com/etl/utils/JDBCUtils.scala
@@ -1,10 +1,11 @@
 package com.etl.utils
 
-import java.sql.Connection
+import java.sql.{Connection, ResultSet, Statement}
 import java.util.Properties
 
 import com.alibaba.druid.pool.DruidDataSourceFactory
 import javax.sql.DataSource
+import org.apache.commons.dbutils.DbUtils
 
 import scala.io.Source
 
@@ -15,4 +16,10 @@ class JDBCUtils {
   source = DruidDataSourceFactory.createDataSource(properties)
 
   def getConnection: Connection = source.getConnection
+
+  def closeConnect(connection: Connection, statement: Statement, resultSet: ResultSet): Unit = {
+    DbUtils.closeQuietly(connection)
+    DbUtils.closeQuietly(statement)
+    DbUtils.closeQuietly(resultSet)
+  }
 }
