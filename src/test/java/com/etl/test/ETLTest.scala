3853a9022b171d2082a62d0de9231c301d6f9aa5
魏喜明 <664651151@qq.com>
2020-01-08 02:01:34 +0800

    2020-01-08 适应性修改

--- src/test/java/com/etl/test/ETLTest.scala
+++ src/test/java/com/etl/test/ETLTest.scala
@@ -1,6 +1,6 @@
 package com.etl.test
 
-import com.etl.common.utils.JDBCUtils
+import com.etl.common.utils.DBUtils
 import org.apache.log4j.Logger
 import org.junit.jupiter.api.Test
 
@@ -8,18 +8,18 @@ class ETLTest {
   private val logger: Logger = Logger.getLogger(this.getClass)
 
   logger.debug("创建JDBCUtils对象")
-  private val jDBCUtils = new JDBCUtils()
+  private val dbUtils = new DBUtils()
 
   logger.debug("运行测试任务ConnectTest")
 
   @Test
   def ConnectTest(): Unit = {
     logger.info("开始获取连接")
-    println(jDBCUtils.getConnection)
+    println(dbUtils.getConnection)
     logger.error("获取连接完成")
   }
 
   //  def closeTest(): Unit = {
-  //    jDBCUtils.closeConnect()
+  //    dbUtils.closeConnect()
   //  }
 }
