2e917d91f87b08ea3c116906c235f368721b8a5d
魏喜明 <664651151@qq.com>
2020-01-07 19:52:59 +0800

    2020-01-07 增加了log4j的测试

--- src/test/java/com/etl/ETLTest.scala
+++ src/test/java/com/etl/ETLTest.scala
@@ -1,18 +1,25 @@
 package com.etl
 
 import com.etl.utils.JDBCUtils
+import org.apache.log4j.Logger
 import org.junit.jupiter.api.Test
 
 class ETLTest {
+  private val logger: Logger = Logger.getLogger(this.getClass)
 
+  logger.debug("创建JDBCUtils对象")
   private val jDBCUtils = new JDBCUtils()
+
+  logger.debug("运行测试任务ConnectTest")
 
   @Test
   def ConnectTest(): Unit = {
+    logger.info("开始获取连接")
     println(jDBCUtils.getConnection)
+    logger.error("获取连接完成")
   }
 
-//  def closeTest(): Unit = {
-//    jDBCUtils.closeConnect()
-//  }
+  //  def closeTest(): Unit = {
+  //    jDBCUtils.closeConnect()
+  //  }
 }
