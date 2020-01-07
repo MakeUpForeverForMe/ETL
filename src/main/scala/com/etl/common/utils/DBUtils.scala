dee38295a7cbda4df814b14d6473ff23a3405b5f
魏喜明 <664651151@qq.com>
2020-01-08 02:01:08 +0800

    2020-01-08 更名JDBCUtils为DBUtils，并修改了close的方式

---
+++ src/main/scala/com/etl/common/utils/DBUtils.scala
@@ -0,0 +1,41 @@
+package com.etl.common.utils
+
+import java.sql.{Connection, ResultSet, Statement}
+import java.util.Properties
+
+import com.alibaba.druid.pool.DruidDataSourceFactory
+import com.alibaba.druid.util.JdbcUtils
+import javax.sql.DataSource
+
+import scala.io.Source
+
+object DBUtils extends DBUtils
+
+sealed class DBUtils {
+  private var source: DataSource = _
+  private val properties = new Properties()
+  properties.load(Source.fromFile(this.getClass.getClassLoader.getResource("jdbc.properties").getPath).bufferedReader())
+  source = DruidDataSourceFactory.createDataSource(properties)
+
+  def getDataSource: DataSource = source
+
+  def getConnection: Connection = source.getConnection
+
+  def close(connection: Connection, statement: Statement, resultSet: ResultSet): Unit = closeBase(connection, statement, resultSet)
+
+  def close(connection: Connection, statement: Statement): Unit = close(connection, statement, null)
+
+  def close(statement: Statement, resultSet: ResultSet): Unit = close(null, statement, resultSet)
+
+  def close(connection: Connection): Unit = JdbcUtils.close(connection)
+
+  def close(statement: Statement): Unit = JdbcUtils.close(statement)
+
+  def close(resultSet: ResultSet): Unit = JdbcUtils.close(resultSet)
+
+  private def closeBase(connection: Connection, statement: Statement, resultSet: ResultSet): Unit = {
+    close(resultSet)
+    close(statement)
+    close(connection)
+  }
+}
