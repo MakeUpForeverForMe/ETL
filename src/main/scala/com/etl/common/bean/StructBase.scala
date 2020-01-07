302be2adb1b425f3bbcb62787d5c40e24d1937be
魏喜明 <664651151@qq.com>
2020-01-07 21:35:54 +0800

    2020-01-07 对bean对象重写了toString方法

--- src/main/scala/com/etl/common/bean/StructBase.scala
+++ src/main/scala/com/etl/common/bean/StructBase.scala
@@ -6,11 +6,13 @@ case class StructBase(
                        id: Long,
                        ctime: Date,
                        utime: Date,
-                       id_type: Int,
+                       idType: Int,
                        alias: String,
                        sourceType: String,
                        hosts: String,
                        user: String,
                        passwd: String,
                        sourcePath: String
-                     ) extends Serializable
+                     ) extends Serializable {
+  override def toString: String = id + "\t" + ctime + "\t" + utime + "\t" + idType + "\t" + alias + "\t" + sourceType + "\t" + hosts + "\t" + user + "\t" + passwd + "\t" + sourcePath
+}
