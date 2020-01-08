76fd74478c5b402d35e67761d4ed1505037755c1
魏喜明 <664651151@qq.com>
2020-01-08 16:06:45 +0800

    2020-01-08 增加注释和方法，并且根据包对象做了简化

--- src/main/scala/com/etl/common/dao/BaseDao.scala
+++ src/main/scala/com/etl/common/dao/BaseDao.scala
@@ -1,14 +1,45 @@
 package com.etl.common.dao
 
 
+import java.sql.Connection
 import java.util
 
 import com.alibaba.druid.util.JdbcUtils
-import com.etl.common.utils.DBUtils
+import com.etl.common._
 import javax.sql.DataSource
 
 class BaseDao {
+  /**
+    * 执行非事务的增、删、改操作
+    *
+    * @param dataSource 需要传入 DataSource 获取连接
+    * @param sql        传入需要执行的SQL
+    * @param args       SQL中对应的变量部分
+    * @return 返回执行成功的受影响行数
+    */
   def update(dataSource: DataSource, sql: String, args: Any*): Int = JdbcUtils.executeUpdate(dataSource, sql, args)
 
-  def execute(dataSource: DataSource, sql: String, args: Any*): util.List[util.Map[String, AnyRef]] = JdbcUtils.executeQuery(dataSource, sql, args)
+  /**
+    * 执行事务性的增、删、改操作
+    *
+    * @param connection 需要传入连接 Connection
+    * @param sql        传入需要执行的SQL
+    * @param args       SQL中对应的变量部分
+    * @return 返回执行成功的受影响行数
+    */
+  def update(connection: Connection, sql: String, args: Any*): Int = {
+    val i = JdbcUtils.executeUpdate(connection, sql, args.asInstanceOf[util.List[AnyRef]])
+    JdbcUtils.close(connection)
+    i
+  }
+
+  /**
+    * 获取数据库数据
+    *
+    * @param dataSource 需要传入 DataSource 获取连接
+    * @param sql        传入需要执行的SQL
+    * @param args       SQL中对应的变量部分
+    * @return 返回获取到的数据
+    */
+  def execute(dataSource: DataSource, sql: String, args: Any*): BufferMapRef = JdbcUtils.executeQuery(dataSource, sql)
 }
