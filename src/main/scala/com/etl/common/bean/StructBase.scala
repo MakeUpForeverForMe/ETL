363d4dbf8f62b152d2e3280d8171a97849dff246
魏喜明 <664651151@qq.com>
2020-01-08 16:08:34 +0800

    2020-01-08 根据表结构做了修改，增加了ResultSetConsumer的测试

--- src/main/scala/com/etl/common/bean/StructBase.scala
+++ src/main/scala/com/etl/common/bean/StructBase.scala
@@ -1,18 +1,55 @@
 package com.etl.common.bean
 
-import java.sql.Date
+import java.sql.{ResultSet, Timestamp}
+
+import com.alibaba.druid.util.ResultSetConsumer
+
 
 case class StructBase(
                        id: Long,
-                       ctime: Date,
-                       utime: Date,
+                       ctime: Timestamp,
+                       utime: Timestamp,
                        idType: Int,
-                       alias: String,
-                       sourceType: String,
+                       useType: String,
                        hosts: String,
                        user: String,
                        passwd: String,
                        sourcePath: String
-                     ) extends Serializable {
-  override def toString: String = id + "\t" + ctime + "\t" + utime + "\t" + idType + "\t" + alias + "\t" + sourceType + "\t" + hosts + "\t" + user + "\t" + passwd + "\t" + sourcePath
+                     ) extends ResultSetConsumer[StructBase] with Serializable {
+  override def toString: String = id + "\t" + ctime + "\t" + utime + "\t" + idType + "\t" + useType + "\t" + hosts + "\t" + user + "\t" + passwd + "\t" + sourcePath
+
+  override def apply(rs: ResultSet): StructBase = ???
+
+  override def accept(`object`: StructBase): Unit = ???
 }
+
+//class StructBase extends ResultSetConsumer[StructBase] with Serializable {
+//  private var id: Long = _
+//  private var ctime: Timestamp = _
+//  private var utime: Timestamp = _
+//  private var idType: Int = _
+//  private var useType: String = _
+//  private var hosts: String = _
+//  private var user: String = _
+//  private var passwd: String = _
+//  private var sourcePath: String = _
+//
+//  def this(id: Long, ctime: Timestamp, utime: Timestamp, idType: Int, useType: String, hosts: String, user: String, passwd: String, sourcePath: String) {
+//    this
+//    this.id = id
+//    this.ctime = ctime
+//    this.utime = utime
+//    this.idType = idType
+//    this.useType = useType
+//    this.hosts = hosts
+//    this.user = user
+//    this.passwd = passwd
+//    this.sourcePath = sourcePath
+//  }
+//
+//  override def toString: String = id + "\t" + ctime + "\t" + utime + "\t" + idType + "\t" + useType + "\t" + hosts + "\t" + user + "\t" + passwd + "\t" + sourcePath
+//
+//  override def apply(rs: ResultSet): StructBase = ???
+//
+//  override def accept(structBase: StructBase): Unit = ???
+//}
