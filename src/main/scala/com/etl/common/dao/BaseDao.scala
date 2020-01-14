0cc6f1a231888980ba7e69370532bee3dfb298aa
魏喜明 <664651151@qq.com>
2020-01-14 15:15:16 +0800

    2020-01-14 删除部分无用方法

--- src/main/scala/com/etl/common/dao/BaseDao.scala
+++ src/main/scala/com/etl/common/dao/BaseDao.scala
@@ -20,26 +20,12 @@ class BaseDao {
   def update(dataSource: DataSource, sql: String, args: Any*): Int = JdbcUtils.executeUpdate(dataSource, sql, args)
 
   /**
-    * 执行事务性的增、删、改操作
-    *
-    * @param connection 需要传入连接 Connection
-    * @param sql        传入需要执行的SQL
-    * @param args       SQL中对应的变量部分
-    * @return 返回执行成功的受影响行数
-    */
-  def update(connection: Connection, sql: String, args: Any*): Int = {
-    val i = JdbcUtils.executeUpdate(connection, sql, args.asInstanceOf[util.List[AnyRef]])
-    JdbcUtils.close(connection)
-    i
-  }
-
-  /**
     * 获取数据库数据
     *
     * @param dataSource 需要传入 DataSource 获取连接
     * @param sql        传入需要执行的SQL
     * @param args       SQL中对应的变量部分
-    * @return 返回获取到的数据
+    * @return 以 List<`Map`> 形式返回获取到的数据
     */
   def execute(dataSource: DataSource, sql: String, args: Any*): BufferMapRef = JdbcUtils.executeQuery(dataSource, sql)
 }
