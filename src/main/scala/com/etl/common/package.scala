a2e9ad9c284aabcf0fc7e044f5ef50a23dd0fae5
魏喜明 <664651151@qq.com>
2020-01-14 15:14:48 +0800

    2020-01-14 修改类型6

--- src/main/scala/com/etl/common/package.scala
+++ src/main/scala/com/etl/common/package.scala
@@ -10,18 +10,13 @@ package object common {
   type JuList = java.util.List[JuMap]
 
   type SMMap = mutable.Map[String, Any]
-  val SMMap: SMMap = mutable.HashMap[String, Any]()
+  val SMMap: Seq[(String, Any)] => mutable.HashMap[String, Any] = mutable.HashMap[String, Any]
 
   type SuMapRef = mutable.Map[String, AnyRef]
 
   type BufferMapRef = mutable.Buffer[SuMapRef]
 
-
   implicit def Map4S2J(sMMap: SMMap): JuMap = sMMap.asInstanceOf[SuMapRef].asJava
 
   implicit def List4S2J(juList: JuList): BufferMapRef = juList.asScala.map(_.asScala)
-
-  def ss(args: Any*): Seq[Any] = args
-
-  //  implicit def
 }
